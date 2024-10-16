9345dbd372a60f1e7b9e2e9525b839f6e37b6c02
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-10-17 02:48:48 +0300

    feat(pt): added reading filetype from arguments

--- pt.c
+++ pt.c
@@ -1,17 +1,23 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "parser.h"
+
+char *check_args(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
+	check_args(argc, argv);
+	char *filepath = argv[1];
+
 	initscr();
 	curs_set(0);
 	noecho();
 
-	int slides_n = get_num_slides("test");
+	int slides_n = get_num_slides(filepath);
 	Slide *slides;
-	parse_file("test", slides);
+	parse_file(filepath, slides);
 
 	int c;
 	int cur_slide_ind = 0;
@@ -34,4 +40,17 @@ int main(int argc, char *argv[])
 	} while ((c = getch()) != 'q');
 	endwin();
 	curs_set(1);
+}
+
+char *check_args(int argc, char *argv[])
+{
+	if (argc < 2) {
+		fprintf(stderr, "need filepath\n");
+		exit(1);
+	}
+
+	if (access(argv[1], F_OK) != 0) {
+		fprintf(stderr, "file doesn't exist\n");
+		exit(1);
+	}
 }
