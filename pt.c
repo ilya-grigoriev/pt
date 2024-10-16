bf696fc28a62197a166557d727c859e264e033a3
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-10-17 02:19:59 +0300

    fix(pt): changed pointers to struct

--- pt.c
+++ pt.c
@@ -1,8 +1,7 @@
 #include <ncurses.h>
+#include <stdlib.h>
 #include <string.h>
 #include "parser.h"
-
-static char **slides;
 
 int main(int argc, char *argv[])
 {
@@ -10,21 +9,28 @@ int main(int argc, char *argv[])
 	curs_set(0);
 	noecho();
 
-	int max_slides_n = parse_file("test", &slides);
-	int cur_slides_n = 1;
+	int slides_n = get_num_slides("test");
+	Slide *slides;
+	parse_file("test", slides);
 
 	int c;
+	int cur_slide_ind = 0;
 	do {
 		switch (c) {
 			case 'l':
-				cur_slides_n += (cur_slides_n + 1 <= max_slides_n) ? 1 : 0;
+				if (cur_slide_ind + 1 < slides_n) cur_slide_ind++;
 				break;
 			case 'h':
-				cur_slides_n -= (cur_slides_n - 1 > 0) ? 1 : 0;
+				if (cur_slide_ind - 1 >= 0) cur_slide_ind--;
 				break;
 		}
 
-		mvprintw(10, 10, "%s", *(slides+cur_slides_n-1));
+		erase();
+		int start = 10;
+		for (int i = 0; i < slides[cur_slide_ind].lines_count; i++, start++) {
+			mvprintw(start, 10, "%s", slides[cur_slide_ind].lines[i]);
+		}
+		refresh();
 	} while ((c = getch()) != 'q');
 	endwin();
 	curs_set(1);
