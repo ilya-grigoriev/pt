258bdae855ed4e5c50940381e4f7632f7a9c4847
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2025-01-31 19:21:22 +0300

    feat(line): added centering text

--- line.c
+++ line.c
@@ -23,13 +23,49 @@ int get_max_slides(Line *lines)
 	return lines->num_slides;
 }
 
-void print_lines(Line *lines, int cur_num_slide)
-{
+Line *skip_slides(Line *lines, int cur_num_slide) {
 	while (lines->num_slides != cur_num_slide)
 		lines = lines->next_line;
+	return lines;
+}
+
+int get_num_slides(Line *lines, int cur_num_slide)
+{
+	int num = 0;
+	while (lines->num_slides == cur_num_slide) {
+		num++;
+		lines = lines->next_line;
+	}
+
+	return num;
+}
+
+int get_max_len_line(Line *lines, int cur_num_slide) {
+	int max_len = 0;
+	while (lines->num_slides == cur_num_slide) {
+		max_len = (max_len < strlen(lines->content)) ? strlen(lines->content) : max_len;
+		lines = lines->next_line;
+	}
+
+	return max_len;
+}
+
+void print_lines(Line *lines, int cur_num_slide)
+{
+	lines = skip_slides(lines, cur_num_slide);
 	
 	mvprintw(LINES-1, COLS-3, "%d", cur_num_slide);
-	for (int x=rows_margin, y=cols_margin; lines->content != NULL && lines->num_slides == cur_num_slide; x++) {
+
+	int x, y;
+	if (is_center) {
+		x = (LINES - get_num_slides(lines, cur_num_slide)) / 2;
+		y = (COLS - get_max_len_line(lines, cur_num_slide)) / 2;
+	}
+	else {
+		x = rows_margin;
+		y = cols_margin;
+	}
+	for (; lines->content != NULL && lines->num_slides == cur_num_slide; x++) {
 		mvprintw(x, y, "%s", lines->content);
 		lines = lines->next_line;
 	}
